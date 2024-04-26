ffcaec942aa1e082227c58ec6f578eb55bfe9b48
yaqui2005 <167486525+yaqui2005@users.noreply.github.com>
2024-04-25 22:09:19 -0500

    comnios en index

--- index.php
+++ index.php
@@ -52,10 +52,10 @@
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="img/hotel1.jpg" class="d-block w-100" alt="50" width="100" height="300">
+                    <img src="img/hotel1.jpg" class="d-block w-100" alt="50" width="50" height="300">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/hotel2.jpg" class="d-block w-100" alt="50" width="100" height="300">
+                    <img src="img/hotel2.jpg" class="d-block w-100" alt="50" width="50" height="300">
                 </div>
                 <!-- Agrega más imágenes aquí si es necesario -->
             </div>
